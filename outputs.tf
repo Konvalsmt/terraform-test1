d018a0af9f4d7070fab5a62ef2aef476bf9ab876
Сергей Коновал <41016789+Konvalsmt@users.noreply.github.com>
2020-10-25 14:32:56 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,5 +9,5 @@ output "public_us_east_1a_id" {
   value = "${module.aws_vpc.public_us_east_1b_id}"
 }
 output "instance_ip_address" {
-  value = "${module.aws_vpc.instance_ip_address}"
+  value = "${module.aws_ec2.instance_ip_address}"
 }
