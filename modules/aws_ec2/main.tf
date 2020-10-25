d6b946c02293db0ea2c339b150298de547734c91
Сергей Коновал <41016789+Konvalsmt@users.noreply.github.com>
2020-10-25 15:25:14 +0200

    Update main.tf

--- modules/aws_ec2/main.tf
+++ modules/aws_ec2/main.tf
@@ -30,21 +30,7 @@ resource "aws_security_group" "main" {
   }
 }
 
-resource "aws_launch_configuration" "web" {
-  name_prefix = "web-"
 
-  image_id = lookup(var.ami_ids, var.region)
-  instance_type = "t2.micro"
-  key_name="virtu"
-
-  security_groups =[aws_security_group.main.id]
-  associate_public_ip_address = true
-  
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
 
 
 resource "aws_instance" "main" {
