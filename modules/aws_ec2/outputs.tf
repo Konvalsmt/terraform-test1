0d686f9ef1c1a7f6ecb53974762a8428afddee47
Сергей Коновал <41016789+Konvalsmt@users.noreply.github.com>
2020-10-25 15:00:20 +0200

    Update outputs.tf

--- modules/aws_ec2/outputs.tf
+++ modules/aws_ec2/outputs.tf
@@ -1,6 +1,6 @@
-output "instance_id" {
-  value = aws_instance.main.id
-}
+#output "instance_id" {
+#  value = aws_instance.main.id
+#}
 output "instance_ip_address" {
   value = aws_eip.EIP.public_ip
 }
