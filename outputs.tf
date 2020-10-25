ffa866120a23165ec7e8d29f2bd1a9149b4d03c9
Сергей Коновал <41016789+Konvalsmt@users.noreply.github.com>
2020-10-25 11:51:48 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
   
 output "my_vpc_id" {
-  value = "${module.aws_vpc.my_vpc.id}"  
+  value = "${module.aws_vpc.my_vpc_id}"  
 }
 output "public_us_east_1b_id" {
-  value ="${module.aws_vpc.aws_subnet.public1.id}"  
+  value ="${module.aws_vpc.public_us_east_1b_id}"  
 }
 output "public_us_east_1a_id" {
-  value = "${module.aws_subnet.public2.id}"
+  value = "${module.aws_vpc.public_us_east_1b_id}"
 }
