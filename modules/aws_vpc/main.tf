35dcac214c63ab1696573076459271e1ca1a6bc4
Сергей Коновал <41016789+Konvalsmt@users.noreply.github.com>
2020-10-25 12:14:46 +0200

    Update main.tf

--- modules/aws_vpc/main.tf
+++ modules/aws_vpc/main.tf
@@ -102,6 +102,13 @@ resource "aws_security_group" "allow_http" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+    ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port       = 0
@@ -115,128 +122,4 @@ resource "aws_security_group" "allow_http" {
   }
 }
 
-
-
-resource "aws_security_group" "elb_http" {
-  name        = "elb_http"
-  description = "Allow HTTP traffic to instances through Elastic Load Balancer"
-  vpc_id = aws_vpc.my_vpc.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "Allow HTTP through ELB Security Group"
-  }
-}
-
-resource "aws_elb" "web_elb" {
-  name = "web-elb"
-  security_groups = [
-    aws_security_group.elb_http.id
-  ]
-  subnets = [
-    aws_subnet.public1.id,
-    aws_subnet.public2.id
-  ]
-
-  cross_zone_load_balancing   = true
-
-  health_check {
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-    timeout = 3
-    interval = 30
-    target = "HTTP:80/"
-  }
-
-  listener {
-    lb_port = 80
-    lb_protocol = "http"
-    instance_port = "80"
-    instance_protocol = "http"
-  }
-
-}
-
-resource "aws_launch_configuration" "launch" {
-  name_prefix   = "my-launch-configuration-"
-  image_id      = "ami-0f86bb438e080dd6b"
-  instance_type = "t2.micro"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_autoscaling_group" "web" {
-  name = "AUTOSCAL-ASG"
-
-  min_size             = 1
-  desired_capacity     = 2
-  max_size             = 4
-  
-  health_check_type    = "ELB"
-  load_balancers = [
-    aws_elb.web_elb.id
-  ]
-
-launch_configuration = "aws_launch_configuration.launch.name"
-
-  enabled_metrics = [
-    "GroupMinSize",
-    "GroupMaxSize",
-    "GroupDesiredCapacity",
-    "GroupInServiceInstances",
-    "GroupTotalInstances"
-  ]
-
-  metrics_granularity = "1Minute"
-
-  vpc_zone_identifier  = [
-    aws_subnet.public1.id,
-    aws_subnet.public2.id
-  ]
-
-  # Required to redeploy without an outage.
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tag {
-    key                 = "Name"
-    value               = "web"
-    propagate_at_launch = true
-  }
-
-}
-
-resource "aws_autoscaling_policy" "web_policy_up" {
-  name = "web_policy_up"
-  scaling_adjustment = 1
-  adjustment_type = "ChangeInCapacity"
-  cooldown = 300
-  autoscaling_group_name = aws_autoscaling_group.web.name
-}
-
-
-
-resource "aws_autoscaling_policy" "web_policy_down" {
-  name = "web_policy_down"
-  scaling_adjustment = -1
-  adjustment_type = "ChangeInCapacity"
-  cooldown = 300
-  autoscaling_group_name = aws_autoscaling_group.web.name
-}
 
