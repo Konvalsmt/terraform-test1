134a5c1051753c5cd8f2d4886522fe9efa1445a7
Сергей Коновал <41016789+Konvalsmt@users.noreply.github.com>
2020-10-25 09:54:18 +0200

    Create outputs.tf

---
+++ modules/aws_vpc/outputs.tf
@@ -0,0 +1,9 @@
+output "my_vpc_id" {
+  value = "${aws_vpc.my_vpc.id}"  
+}
+output "public_us_east_1b_id" {
+  value ="${aws_vpc.aws_subnet.public1.id}"  
+}
+output "public_us_east_1a_id" {
+  value = "${aws_vpc.aws_subnet.public2.id}"
+}
