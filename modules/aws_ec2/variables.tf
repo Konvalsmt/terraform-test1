d1de592b500717ebada88619956d33f16e25df75
Сергей Коновал <41016789+Konvalsmt@users.noreply.github.com>
2020-10-25 12:04:11 +0200

    Update variables.tf

--- modules/aws_ec2/variables.tf
+++ modules/aws_ec2/variables.tf
@@ -1,3 +1,23 @@
 variable "region" {
+     type=string
      default = "us-east-1"
+}
+
+variable "instance_name"  {
+     type=string
+     default = "my-ec2-inst"
+}
+
+variable "ami_ids"  {
+     type=map
+     default = {
+    us-east-1 = "ami-0947d2ba12ee1ff75"
+    us-east-2 = "ami-03657b56516ab7912"
+    eu-west-1 = "ami-0bb3fad3c0286ebd5"
+  }
+}
+
+variable "type_inst" {
+     type=string
+     default = "t2.micro"
 }
