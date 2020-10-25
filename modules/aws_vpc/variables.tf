dc3fbc3e6408c25f399d50eea5235c5f207926cd
Сергей Коновал <41016789+Konvalsmt@users.noreply.github.com>
2020-10-25 10:35:41 +0200

    Update variables.tf

--- modules/aws_vpc/variables.tf
+++ modules/aws_vpc/variables.tf
@@ -1,8 +1,10 @@
 variable "azs" {
-     type=list
+     type=list(string)
+     default=["us-east-1"]
 }
 variable "public_subnets" {
-        type=list
+        type=list(string)
+     default=["10.10.1.0/24"]
 }
 variable "cidr" {
     type =string
@@ -11,6 +13,7 @@ variable "name" {
     type =string
 }
 variable "private_subnets" {
-        type=list
+        type=list(string)
+      default=["10.10.2.0/24"]
 }
 # end of variables.tf
