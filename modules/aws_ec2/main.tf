9e2dda9bf52f376c9ae615784eb50eb63f12cf5b
Сергей Коновал <41016789+Konvalsmt@users.noreply.github.com>
2020-10-25 13:03:09 +0200

    Update main.tf

--- modules/aws_ec2/main.tf
+++ modules/aws_ec2/main.tf
@@ -21,41 +21,11 @@ resource "aws_eip" "main" {
 }
 
 
-resource "aws_security_group" "elb_http" {
-  name        = "elb_http"
-  description = "Allow HTTP traffic to instances through Elastic Load Balancer"
-  vpc_id = var.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-    ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "Allow HTTP through ELB Security Group"
-  }
-}
 
 resource "aws_elb" "web_elb" {
   name = "web-elb"
   security_groups = [
-    aws_security_group.elb_http.id
+    aws_security_group.main.id
   ]
   subnets = [
     var.public_1,
