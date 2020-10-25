f8a832256ee9588c20101a0bd99024cf8714fbd4
Сергей Коновал <41016789+Konvalsmt@users.noreply.github.com>
2020-10-25 13:25:08 +0200

    Update main.tf

--- modules/aws_vpc/main.tf
+++ modules/aws_vpc/main.tf
@@ -91,35 +91,3 @@ resource "aws_route_table_association" "my_vpc_us_east_1b_public" {
     route_table_id = aws_route_table.my_vpc_public.id
 }
 
-resource "aws_security_group" "allow_http" {
-  name        = "allow_http"
-  description = "Allow HTTP inbound connections"
-  vpc_id = aws_vpc.my_vpc.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-    ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "Allow HTTP Security Group"
-  }
-}
-
-
