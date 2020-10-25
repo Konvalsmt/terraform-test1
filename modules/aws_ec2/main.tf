f9a7a6d520d793c80c4f737f426a7ae9ef38cc34
Сергей Коновал <41016789+Konvalsmt@users.noreply.github.com>
2020-10-25 11:56:18 +0200

    Create main.tf

---
+++ modules/aws_ec2/main.tf
@@ -0,0 +1,167 @@
+#aws autoscaling attach-instances --instance-ids i-93633f9b --auto-scaling-group-name my-auto-scaling-group
+
+resource "aws_security_group" "main" {
+  description = "Managed by Terraform"
+  vpc_id      = "${module.aws_vpc.my_vpc_id}"
+}
+
+
+resource "aws_instance" "main" {
+  ami                    = lookup(var.ami_ids, var.region)
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.main.id]
+  name ="Pilots"
+  tags = {
+    Name = "Pilots"
+  }
+  key_name="virtu"
+}
+
+resource "aws_eip" "main" {
+  instance = aws_instance.main.id
+}
+
+
+
+resource "aws_security_group_rule" "out" {
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.main.id
+  cidr_blocks       = ["0.0.0.0/0"]
+  type              = "egress"
+}
+
+resource "aws_security_group_rule" "in" {
+  for_each          = toset(var.ports_to_open)
+  to_port           = each.value
+  from_port         = each.value
+  protocol          = "tcp"
+  security_group_id = aws_security_group.main.id
+  cidr_blocks       = ["0.0.0.0/0"]
+  type              = "ingress"
+}
+
+
+
+resource "aws_security_group" "elb_http" {
+  name        = "elb_http"
+  description = "Allow HTTP traffic to instances through Elastic Load Balancer"
+  vpc_id = "${module.aws_vpc.my_vpc_id}"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+    ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Allow HTTP through ELB Security Group"
+  }
+}
+
+resource "aws_elb" "web_elb" {
+  name = "web-elb"
+  security_groups = [
+    aws_security_group.elb_http.id
+  ]
+  subnets = [
+    aws_subnet.public_us_east_1a.id,
+    aws_subnet.public_us_east_1b.id
+  ]
+
+  cross_zone_load_balancing   = true
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:80/"
+  }
+
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = "80"
+    instance_protocol = "http"
+  }
+
+}
+
+resource "aws_autoscaling_group" "web" {
+  name = "Instan-EC2-asg"
+
+  min_size             = 1
+  desired_capacity     = 2
+  max_size             = 4
+  
+  health_check_type    = "ELB"
+  load_balancers = [
+    aws_elb.web_elb.id
+  ]
+
+# launch_configuration = aws_launch_configuration.web.name
+
+  enabled_metrics = [
+    "GroupMinSize",
+    "GroupMaxSize",
+    "GroupDesiredCapacity",
+    "GroupInServiceInstances",
+    "GroupTotalInstances"
+  ]
+
+  metrics_granularity = "1Minute"
+
+  vpc_zone_identifier  = [
+    aws_subnet.public_us_east_1a.id,
+    aws_subnet.public_us_east_1b.id
+  ]
+
+  # Required to redeploy without an outage.
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "web"
+    propagate_at_launch = true
+  }
+
+}
+
+resource "aws_autoscaling_policy" "web_policy_up" {
+  name = "web_policy_up"
+  scaling_adjustment = 1
+  adjustment_type = "ChangeInCapacity"
+  cooldown = 300
+  autoscaling_group_name = aws_autoscaling_group.web.name
+}
+
+
+
+resource "aws_autoscaling_policy" "web_policy_down" {
+  name = "web_policy_down"
+  scaling_adjustment = -1
+  adjustment_type = "ChangeInCapacity"
+  cooldown = 300
+  autoscaling_group_name = aws_autoscaling_group.web.name
+}
+
+
