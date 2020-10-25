710bf2277913aea2b802760b643e9620b5698d15
Сергей Коновал <41016789+Konvalsmt@users.noreply.github.com>
2020-10-25 09:53:28 +0200

    Create variables.tf

---
+++ modules/aws_vpc/variables.tf
@@ -0,0 +1,16 @@
+variable "azs" {
+     type=list
+}
+variable "public_subnets" {
+        type=list
+}
+variable "cidr" {
+    type =string
+}
+variable "name" {
+    type =string
+}
+variable "private_subnets" {
+        type=list
+}
+# end of variables.tf
