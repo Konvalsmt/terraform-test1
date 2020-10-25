9240192f13b689ee5000ded11b27e8a378725ab8
Сергей Коновал <41016789+Konvalsmt@users.noreply.github.com>
2020-10-25 15:36:58 +0200

    Update main.tf

--- modules/aws_ec2/main.tf
+++ modules/aws_ec2/main.tf
@@ -36,9 +36,11 @@ resource "aws_security_group" "main" {
 resource "aws_instance" "main" {
   ami                    = lookup(var.ami_ids, var.region)
   instance_type          = "t2.micro"
+  security_groups=["allow_http"]
   vpc_security_group_ids = [aws_security_group.main.id]
+  subnet_id  = var.subnet_id
   tags = {
-    Name = "Pilots"
+    Name = var.instance_name
   }
   key_name="virtu"
 }
@@ -50,5 +52,9 @@ resource "aws_eip" "EIP" {
   depends_on                = [var.igw]
 }
 
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.main.id
+  allocation_id = aws_eip.EIP.id
+}
 
 
