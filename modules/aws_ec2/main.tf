fdad56a0ad3e33c2b6915c3e48183646e107de6f
Сергей Коновал <41016789+Konvalsmt@users.noreply.github.com>
2020-10-27 12:47:25 +0200

    Update main.tf

--- modules/aws_ec2/main.tf
+++ modules/aws_ec2/main.tf
@@ -71,6 +71,7 @@ resource "aws_launch_configuration" "as_conf" {
 resource "aws_autoscaling_group" "my_asg" {
   name                 = "terraform-asg-example"
   launch_configuration = aws_launch_configuration.as_conf.name
+  vpc_zone_identifier  = [var.public_1, var.public_2]
   min_size             = 1
   max_size             = 2
 
